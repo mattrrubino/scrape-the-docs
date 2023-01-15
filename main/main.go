a3055f379158fcaa997f2fdf4a0852d640b7b9da
Matthew Rubino <mrrubino@buffalo.edu>
2023-01-15 01:19:57 -0500

    Fix typo

--- main/main.go
+++ main/main.go
@@ -9,7 +9,7 @@ import (
 	"matthewrubino.com/scrape/scrape"
 )
 
-var maxDepth = flag.Int("d", 3, "Specify max default isepth of the crawler. Default is 3.")
+var maxDepth = flag.Int("d", 3, "Specify max default depth of the crawler. Default is 3.")
 var maxGoroutines = flag.Int("c", 3, "Specify max number of concurrent requests. Default is 3.")
 
 func main() {
